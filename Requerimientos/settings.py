45a3ef4035a6a0a94cc60152eded39e83480c8c5
israel109910 <israel109910@gmail.com>
2025-06-25 11:51:25 -0600

    Add psycopg2-binary to requirements agrgar s3

--- Requerimientos/settings.py
+++ Requerimientos/settings.py
@@ -110,13 +110,18 @@ AUTH_USER_MODEL = 'codigos.User'
 
 # Almacenamiento S3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID       = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY   = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = 'us-east-2'
-AWS_QUERYSTRING_AUTH = True
-AWS_S3_FILE_OVERWRITE = True
-AWS_DEFAULT_ACL = None
+
+AWS_S3_REGION_NAME      = 'us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_ADDRESSING_STYLE  = 'virtual'
+
+AWS_QUERYSTRING_AUTH    = True
+AWS_S3_FILE_OVERWRITE   = True
+AWS_DEFAULT_ACL         = None
+
 
 # CSRF para Railway
 CSRF_TRUSTED_ORIGINS = [
