ce8834dec6bb238c62031f405f6079c83c56571a
israel109910 <israel109910@gmail.com>
2025-07-07 12:55:03 -0600

    cambio de base de datos

--- Requerimientos/settings.py
+++ Requerimientos/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Seguridad
 SECRET_KEY = '+bh4@(y1sbv2c#+1p179=$fgj-oht55g0k3o!0c0wo#hz#y%&q'
-DEBUG = False  # Cambia a False en producción
+DEBUG = True  # Cambia a False en producción
 
 ALLOWED_HOSTS = ['*', 'instrumentosiiesa-production.up.railway.app', 'localhost']
 
@@ -60,7 +60,9 @@ WSGI_APPLICATION = 'Requerimientos.wsgi.application'
 
 # Base de datos
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')  # Aquí 'DATABASE_URL' es el nombre de la variable
+    )
 }
 
 #{
