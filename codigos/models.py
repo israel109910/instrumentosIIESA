84926417dfb57df697dd7a325b6b2d784dc44b53
israel109910 <israel109910@gmail.com>
2025-07-11 12:35:23 -0600

    version final 11-07

--- codigos/models.py
+++ codigos/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 import uuid
+from django.utils.text import slugify
+
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -32,6 +34,19 @@ class Laboratorio(models.Model):
         return self.nombre
 
 
+# Función para guardar el certificado con nombre personalizado
+def ruta_certificado(instance, filename):
+    nombre = slugify(instance.nombre)  # Ej: "termometro-uno"
+    folio = slugify(instance.folio or "sin-folio")  # Evita errores si folio es None
+
+    # Obtener extensión del archivo original
+    extension = os.path.splitext(filename)[1]  # por ejemplo: '.pdf'
+    nuevo_nombre = f"certificado_{folio}{extension}"  # certificado_f-001.pdf
+
+    carpeta = f"{nombre}-{folio}"  # certificados/termometro-uno-f-001/
+    return os.path.join('certificados', carpeta, nuevo_nombre)
+
+
 class Instrumento(models.Model):
     MAGNITUD_CHOICES = [
         ('temperatura', 'Temperatura'),
@@ -44,14 +59,13 @@ class Instrumento(models.Model):
     tag = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     serie = models.CharField(max_length=100)
-    folio = models.CharField(max_length=100)
+    folio = models.CharField(max_length=100, null=True, blank=True)
     fecha_calibracion = models.DateField()
     quien_calibro = models.CharField(max_length=100, null=True, blank=True)
-    certificado = models.FileField(upload_to='certificados/', null=True, blank=True)
+    certificado = models.FileField(upload_to=ruta_certificado, null=True, blank=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     magnitud = models.CharField(max_length=20, choices=MAGNITUD_CHOICES, default='temperatura')
     laboratorio = models.ForeignKey('Laboratorio', on_delete=models.CASCADE)
-
 
     def __str__(self):
         return f"{self.nombre} - {self.tag}"
--- codigos/urls.py
+++ codigos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.http import HttpResponse
+from django.contrib.auth.views import LogoutView
 from . import views
 urlpatterns = [
 
@@ -12,4 +13,6 @@ urlpatterns = [
     path('instrumento/<int:instrumento_id>/qr_pdf/', views.qr_pdf, name='qr_pdf'),
     path('validar/<uuid:uuid>/', views.validar_certificado, name='validar_certificado'),
     path('unauthorized/', views.unauthorized, name='unauthorized'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
