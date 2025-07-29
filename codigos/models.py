5bb42c12275ec3b4f50d2cf0c92abc0254598dc7
IIESA <jquevedoo@iiesa-solutions.com.mx>
2025-07-29 13:05:12 -0600

    Seccion de viaticos 29/07/2025

--- codigos/models.py
+++ codigos/models.py
@@ -71,3 +71,48 @@ class Instrumento(models.Model):
     def __str__(self):
         return f"{self.nombre} - {self.tag}"
 
+class Sitio(models.Model):
+    nombre = models.CharField(max_length=100)
+    distancia_km = models.DecimalField(max_digits=6, decimal_places=2)  # ida y vuelta
+    seccion = models.ForeignKey('Seccion', on_delete=models.CASCADE, related_name='sitios')
+
+    class Meta:
+        verbose_name_plural = "Viaticos - Sitios"
+
+    def __str__(self):
+        return self.nombre
+
+class Estado(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Viaticos - Estados"
+
+    def __str__(self):
+        return self.nombre
+
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=100)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, related_name='secciones')
+
+    class Meta:
+        verbose_name_plural = "Viaticos - Secciones"
+
+    def __str__(self):
+        return self.nombre
+
+class ParametrosGlobales(models.Model):
+    precio_gasolina = models.DecimalField(max_digits=5, decimal_places=2)  # Precio por litro
+    rendimiento_km_l = models.DecimalField(max_digits=5, decimal_places=2)  # km/l base
+    peso_herramienta = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)  # en kg
+    peso_prom_persona = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)  # en kg
+    factor_penalizacion = models.DecimalField(max_digits=5, decimal_places=2, default=0.5) 
+    herramientas = models.BooleanField(default=False)  # Factor de penalización
+    cantidad_personas = models.IntegerField(default=1)  # Cantidad de personas por sitio
+    dias_trabajo = models.IntegerField(default=1)  # Días de trabajo por semana
+
+    class Meta:
+        verbose_name_plural = "Viaticos - Parámetros Globales"
+        
+    def __str__(self):
+        return "Parámetros Globales"
